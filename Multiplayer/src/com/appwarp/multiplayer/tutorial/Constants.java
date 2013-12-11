0167c6c87b2ca1b258e08615748e539997c4cf7f
Laurens Verspeek <laurens_verspeek@hotmail.com>
2013-12-11 20:08:36 +0100

    Multiplayer

---
+++ Multiplayer/src/com/appwarp/multiplayer/tutorial/Constants.java
@@ -0,0 +1,15 @@
+package com.appwarp.multiplayer.tutorial;
+
+public class Constants {
+
+	public static String apiKey = "64b70b68e20490cbd3383f8d9c844a4ffd7baf9b9f6e2337006e9ac8cd825c49";
+    public static String secretKey =  "558a6bedf1245a6b183d2977a95497ec240a1072aba900a7b5409d60fc31a932";
+    
+    public static final int BananaId = 1;
+    public static final int GrapeId = 2;
+    public static final int PinaappleId = 3;
+    public static final int StrawaryId = 4;
+    
+    public static final int MonsterSpeed = 4000;	// px per second
+    
+}
